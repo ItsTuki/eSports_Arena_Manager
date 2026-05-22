f185a7c628df8e7840d679bcc625c7c4f9c495cf
Victor <vict.guerra@duocuc.cl>
2026-05-22 18:43:03 -0400

    avance de las clases de user xd

---
+++ user-service/src/main/java/com/example/userservice/clients/UserClient.java
@@ -0,0 +1,4 @@
+package com.example.userservice.clients;
+
+public interface UserClient {
+}
--- user-service/src/main/java/com/example/userservice/repositories/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.models.User;
 import org.springframework.stereotype.Repository;
-import org.springframework.repository.JpaRepository;
+
+
+
 @Repository
-public interface UserRepository extends JpaReposiroty<User,long> {
+public interface UserRepository  {
 }
