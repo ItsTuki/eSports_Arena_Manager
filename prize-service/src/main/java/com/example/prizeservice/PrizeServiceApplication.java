e79bb1bec3f4b21de39cd362e4127deefbbf3b41
Victor <vict.guerra@duocuc.cl>
2026-05-22 16:54:57 -0400

    creacion de microservicios

---
+++ prize-service/src/main/java/com/example/prizeservice/PrizeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.prizeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrizeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrizeServiceApplication.class, args);
+    }
+
+}
---
+++ prize-service/src/test/java/com/example/prizeservice/PrizeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prizeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrizeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
